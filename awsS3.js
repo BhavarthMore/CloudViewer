3af182564f767b6c65d67922f9da7d068d030957
Bhavarth More <125486990+BhavarthMore@users.noreply.github.com>
2024-08-24 14:26:05 +0530

    Add files via upload

---
+++ awsS3.js
@@ -0,0 +1,12 @@
+
+
+const awsS3 = async () => {
+    global.AWS_ACCESS_KEY_ID = 'AKIAQE3RONDMIAQ5FCPL';
+    global.AWS_SECRET_ACCESS_KEY = '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';
+    global.AWS_REGION = 'ap-south-1';
+    global.AWS_BUCKET_NAME = 'shareease1';
+
+    
+}
+
+module.exports = awsS3;
