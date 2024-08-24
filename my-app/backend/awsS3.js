278c670798c3150a19a42dafbffe055b1d6d13b7
Bhavarth More <asmore7.sonu7@gmail.com>
2024-08-24 13:40:39 +0530

    Add existing file

--- my-app/backend/awsS3.js
+++ my-app/backend/awsS3.js
@@ -1,10 +1,10 @@
 
 
 const awsS3 = async () => {
-    global.AWS_ACCESS_KEY_ID = 'AKIAQE3RONDMIAQ5FCPL';
-    global.AWS_SECRET_ACCESS_KEY = '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';
-    global.AWS_REGION = 'ap-south-1';
-    global.AWS_BUCKET_NAME = 'shareease1';
+    global.AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID';
+    global.AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY';
+    global.AWS_REGION = 'AWS_REGION';
+    global.AWS_BUCKET_NAME = 'AWS_BUCKET_NAME';
 
     
 }
